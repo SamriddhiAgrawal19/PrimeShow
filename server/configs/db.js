a6e800eedb93fb9c9b2fc875196ba6b49866020c
Samriddhi Agrawal <ac.samriddhi05@gmail.com>
2025-10-12 00:14:41 +0530

    Connection fixed

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
- 
-const connectDB = async()=>{
-    try{
-        mongoose.connection.on('connected', ()=>console.log('Database Connected'));
-        await mongoose.connect(process.env.MONGODB_URI);
-    }catch(error){
-        console.error('Database Connection Failed:', error);
-        process.exit(1);
-    }
-}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Database Connected to Atlas');
+  } catch (error) {
+    console.error('Database Connection Failed:', error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
